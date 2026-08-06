4c74b4d97fd093f803ccf8ee676efff3a88d27b5
xiaochen66666 <150781778+xiaochen66666@users.noreply.github.com>
2026-08-06 10:12:25 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 #
-# Copyright (c) 2020-2024 JE668 <https://github.com/JE668>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# File name: beta/diy-part2.sh
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-echo 'Modify default IP'
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 取消Lean大登陆密码
+# sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+
+# 修改root密码
+password=$(openssl passwd -1 'admin')
+sed -i "s|root::0:0:99999:7:::|root:$password:0:0:99999:7:::|g" package/base-files/files/etc/shadow
+
+# 取消原主题luci-theme-bootstrap 为默认主题 - 修改 argon 为默认主题 - 删除原默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+rm -rf package/lean/luci-theme-bootstrap
 
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
@@ -30,7 +43,7 @@ sed -i 's/\/var\/upnp.leases/\/tmp\/upnp.leases/g' feeds/packages/net/miniupnpd/
 cat feeds/packages/net/miniupnpd/files/upnpd.config |grep upnp_lease_file
 echo '=========Alert upnp binding file directory!========='
 
-添加主页的CPU温度显示
-sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
-echo "Add CPU Temperature in Admin Index OK====================="
+# 添加主页的CPU温度显示
+# sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
+# echo "Add CPU Temperature in Admin Index OK====================="
